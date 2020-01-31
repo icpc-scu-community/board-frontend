55c3eac48431884ea26a66a365570cb42e986a5e
Amr Salama <amr.mo.salama@gmail.com>
2020-01-31 21:49:19 +0200

    feat: trainee list

---
+++ src/Components/TraineeList/TraineeList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Trainee } from '../Trainee';
+import './styles.css';
+
+class TraineeList extends React.Component {
+  render() {
+    const { trainees } = this.props;
+
+    return (
+      <div className="trainee-list">
+        <table className="trainee-list-table">
+          <thead className="header">
+            <tr>
+              <td>Trainee</td>
+              <td>Progress</td>
+            </tr>
+          </thead>
+          <tbody>
+            {trainees.map(({ name, handle, states }) => (
+              <tr key={handle}>
+                <td className="trainee-cell">
+                  <Trainee name={name} handle={handle} states={states} />
+                </td>
+                <td>
+                  <div className="trainee-progress">
+                    <div className="percentage">33%</div>
+                    <div className="counts">(46/999)</div>
+                    <div className="advance" />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+TraineeList.propTypes = {
+  trainees: PropTypes.array.isRequired
+};
+
+export default TraineeList;
