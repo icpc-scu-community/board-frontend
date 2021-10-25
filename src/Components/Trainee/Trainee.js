b1bc07b7972805ce3301ec77d162f16b88d248ac
Amr Salama <amr.mo.salama@gmail.com>
2021-10-25 23:43:04 +0200

    Revert "feat: use trainees photos coming from codeforces"
    This reverts commit a4eb679676d7b40a468ed925289ff4dabc5a39dd.

--- src/Components/Trainee/Trainee.js
+++ src/Components/Trainee/Trainee.js
@@ -5,22 +5,17 @@ import styles from './styles';
 
 class Trainee extends React.Component {
   render() {
-    const { name, handle, states, problemsCount, photo } = this.props;
+    const { name, handle, states, problemsCount } = this.props;
     const shortName = getShortName(name);
     const colorPercent = getUniqueHashFromName(name);
     const { solved, tried, submissions } = states;
     const missed = problemsCount - (solved + tried);
 
-    const hasPhoto = photo && photo !== 'https://userpic.codeforces.org/no-title.jpg';
-    const withoutPhotoStyle = { backgroundColor: `hsl(${colorPercent * 360}, 100%, 75%)` };
-    const withPhotoStyle = { backgroundImage: `url(${photo})` };
-    const photoStyle = hasPhoto ? withPhotoStyle : withoutPhotoStyle;
-
     return (
       <>
         <div className="trainee">
-          <div className="photo" style={photoStyle}>
-            {!hasPhoto && shortName}
+          <div className="photo" style={{ backgroundColor: `hsl(${colorPercent * 360}, 100%, 75%)` }}>
+            {shortName}
           </div>
           <div className="info">
             <div className="name">{name.toLowerCase().trim()}</div>
@@ -64,7 +59,6 @@ Trainee.propTypes = {
     submissions: PropTypes.number.isRequired,
   }).isRequired,
   problemsCount: PropTypes.number.isRequired,
-  photo: PropTypes.string,
 };
 
 export default Trainee;
